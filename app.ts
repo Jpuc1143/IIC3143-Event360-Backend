e07dd53553ac21b66018f73b42bd9d0bcf2cde91
Jpuc1143 <jaime.perez@uc.cl>
2023-10-11 16:54:08 -0300

    Chore: install eslint and prettier

--- app.ts
+++ app.ts
@@ -2,10 +2,10 @@ import Koa from "koa";
 import bodyParser from "@koa/bodyparser";
 import { router } from "./routers";
 
-const app = new Koa()
-app.listen(3000)
+const app = new Koa();
+app.listen(3000);
 
-app.use(bodyParser())
+app.use(bodyParser());
 
-app.use(router.routes())
+app.use(router.routes());
 app.use(router.allowedMethods());
--- routers/index.ts
+++ routers/index.ts
@@ -3,6 +3,6 @@ import Router from "@koa/router";
 export const router = new Router();
 
 router.get("/", async (ctx, next) => {
-	ctx.body = { hello: "world!" };
-})
-
+  ctx.body = { hello: "world!" };
+  await next();
+});
