f8947b42e162fa80201892cca24e17cac09ebb1b
Jpuc1143 <jaime.perez@uc.cl>
2023-10-14 16:58:15 -0300

    workflow correct test

---
+++ test.ts
@@ -0,0 +1,11 @@
+import Koa from "koa";
+import bodyParser from "@koa/bodyparser";
+import { router } from "./routers";
+
+const app = new Koa();
+app.listen(3000);
+
+app.use(bodyParser());
+
+app.use(router.routes());
+app.use(router.allowedMethods());
