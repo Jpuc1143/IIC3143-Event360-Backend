80efc5b03c9946288fb816ec5fb9e3cb3dde6859
Jpuc1143 <jaime.perez@uc.cl>
2023-10-14 17:22:06 -0300

    Workflow fail test

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import Koa from "koa";
 import bodyParser from "@koa/bodyparser";
 import { router } from "./routers";
 
-const app = new Koa();
+let app = new Koa();
 app.listen(3000);
 
 app.use(bodyParser());
