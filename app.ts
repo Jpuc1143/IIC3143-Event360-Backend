51d332ef594cf3018dcc25e76a0980fd0c2aba52
Jpuc1143 <jaime.perez@uc.cl>
2023-11-17 19:10:09 -0300

    Feat: database configuration

--- app.ts
+++ app.ts
@@ -2,14 +2,30 @@ import Koa from "koa";
 import bodyParser from "@koa/bodyparser";
 import { router } from "./routers";
 import { router as usersRouter } from "./routers/users";
+import { Sequelize } from "sequelize";
+import { config } from "./config/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = new Koa();
 app.listen(3000);
 
-app.use(bodyParser());
+console.log(config[process.env.NODE_ENV]);
+console.log(process.env.NODE_ENV);
+const sequelize = new Sequelize(config[process.env.NODE_ENV]);
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+sequelize
+  .authenticate()
+  .then(() => {
+    app.use(bodyParser());
 
-app.use(usersRouter.routes());
-app.use(usersRouter.allowedMethods());
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+
+    app.use(usersRouter.routes());
+    app.use(usersRouter.allowedMethods());
+  })
+  .catch((error) =>
+    console.error("No se pudo conectar a la base de datos:", error),
+  );
---
+++ config/config.ts
@@ -0,0 +1,17 @@
+export const config = {
+  development: {
+    dialect: "sqlite",
+    storage: "db.sqlite",
+  },
+  test: {
+    dialect: "sqlite",
+    storage: "db.sqlite",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
