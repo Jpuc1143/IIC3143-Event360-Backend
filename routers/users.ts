73e4e0907398754e23c13cdb87571489cd5d2e80
Jpuc1143 <jaime.perez@uc.cl>
2023-11-18 19:44:03 -0300

    Chore: add user event placeholder routes

--- routers/users.ts
+++ routers/users.ts
@@ -16,12 +16,25 @@ router.get("/me", async (ctx, next) => {
   ctx.response.body = user;
   await next();
 });
+
 router.get("/:id", async (ctx, next) => {
   const user = await User.findByPk(ctx.params.id);
   if (user === null) {
     ctx.throw(404, "Usuario no encontrado");
   }
   ctx.response.body = user;
+  await next();
+});
+
+router.get("/:id/events", async (ctx, next) => {
+  // TODO: implement
+  ctx.response.body = [];
+  await next();
+});
+
+router.get("/:id/events_organized", async (ctx, next) => {
+  // TODO: implement
+  ctx.response.body = [];
   await next();
 });
 
