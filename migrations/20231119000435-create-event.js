3abeb4de3314a69956b95ab731c5f7c74e24ee46
Benjamin Caro <bcaro@uc.cl>
2023-11-20 20:50:47 -0300

    feat: Add Events model, migration and routes

---
+++ migrations/20231119000435-create-event.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable("Events", {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.UUID,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      merchantCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable("Events");
+  }
+};
---
+++ models/event.ts
@@ -0,0 +1,35 @@
+import { UUID } from "crypto";
+import { Table, Column, Model, Default, DataType } from "sequelize-typescript";
+
+@Table
+export default class Event extends Model {
+  @Column({ primaryKey: true })
+  @Default(DataType.UUIDV4)
+  id: UUID;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  description!: string;
+
+  @Column({
+    type: DataType.DATE,
+    allowNull: false,
+  })
+  startDate!: Date;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  merchantCode!: string;
+
+}
