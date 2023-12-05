ca3207d181261e11913c98337e5d1a9fee903804
agonzd <agonzd@uc.cl>
2023-12-05 19:05:15 -0300

    Refactor: changed how to check tickets left

--- models/ticketType.ts
+++ models/ticketType.ts
@@ -11,6 +11,7 @@ import {
 } from "sequelize-typescript";
 import Ticket from "./ticket";
 import Event from "./event";
+import { Op } from "sequelize";
 
 @Table
 export default class TicketType extends Model {
@@ -42,4 +43,14 @@ export default class TicketType extends Model {
 
   @HasMany(() => Ticket)
   tickets!: Ticket[];
+
+  async getTicketsLeft(): Promise<number> {
+    const ticketsBought = await Ticket.findAndCountAll({
+      where: {
+        ticketTypeId: this.id,
+        status: { [Op.or]: ["approved", "pending"] },
+      },
+    });
+    return this.amount - ticketsBought.count;
+  }
 }
